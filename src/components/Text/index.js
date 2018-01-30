552825ef07217497062d2e540878efd4d9937d94
Zach Sherman <zksherm@gmail.com>
2018-01-30 10:24:50 -0500

    add options to text

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -1,6 +1,14 @@
 import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
-import { space, borderRadius, color, alignSelf } from 'styled-system'
+import {
+  space,
+  borderRadius,
+  color,
+  alignSelf,
+  fontWeight,
+  letterSpacing,
+  lineHeight
+} from 'styled-system'
 
 const Text = styled.span`
   padding: ${props => props.theme.sizes.paddings[props.size]}rem;
@@ -17,6 +25,9 @@ const Text = styled.span`
   ${color}
   ${space}
   ${alignSelf}
+  ${fontWeight}
+  ${letterSpacing}
+  ${lineHeight}
 `
 
 export default Text
