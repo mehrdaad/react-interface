0f8aa466053a01cb37ee044508c2f85bfb1dade3
Zach Sherman <zksherm@gmail.com>
2018-02-14 11:09:30 -0500

    add tooltip

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,7 +4,7 @@ var createNwbWebpackConfig = require('create-nwb-webpack-config');
 module.exports = {
   webpackConfig: createNwbWebpackConfig(),
   components: 'src/**/[A-Z]*.js',
-  ignore: ['**/components/**/\*Wrapper\.js'],
+  ignore: ['**/components/**/\*Wrapper\.js', '**/components/**/Tip.js'],
   skipComponentsWithoutExample: true,
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/styleguide/ThemeWrapper')
