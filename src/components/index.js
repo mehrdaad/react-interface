ce924a7e551242818ac73cbad68c56e2baa003af
Zach Sherman <zksherm@gmail.com>
2018-03-13 11:18:49 -0400

    fix spinner import

--- src/components/index.js
+++ src/components/index.js
@@ -14,6 +14,7 @@ import MultiSelect from './MultiSelect'
 import Notification from './Notification'
 import Tooltip from './Tooltip'
 import Text from './Text'
+import Spinner from './Spinner'
 import { Menu, MenuItem, MenuHeader, MenuDivider } from './Menu'
 import { Table, TBody, THead, Header, Row, Cell } from './FlexTable'
 
